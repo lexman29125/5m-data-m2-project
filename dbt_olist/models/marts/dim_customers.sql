106f91ff6b22408199429a3aac593ee0bc2b5439
sc-nun216 <215167123+sc-nun216@users.noreply.github.com>
2025-09-05 19:56:46 +0800

    Update dim_customers.sql

--- dbt_olist/models/marts/dim_customers.sql
+++ dbt_olist/models/marts/dim_customers.sql
@@ -5,8 +5,9 @@ select distinct
     c.customer_unique_id,
     c.customer_city,
     c.customer_state,
-    g.geolocation_id,
+    g.latitude,
+    g.longitude,
     current_timestamp as record_loaded_at
 from {{ ref('stg_customers') }} c
-left join {{ ref('dim_geolocation') }} g
+left join {{ ref('stg_geolocation') }} g
   on c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
