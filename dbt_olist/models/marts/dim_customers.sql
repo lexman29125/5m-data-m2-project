548dcac46be721d1f1ff899eb61be48fe630ca31
Sharon-C <215167123+sc-nun216@users.noreply.github.com>
2025-09-09 19:46:56 +0800

    update dbt models

--- dbt_olist/models/marts/dim_customers.sql
+++ dbt_olist/models/marts/dim_customers.sql
@@ -5,10 +5,10 @@ SELECT DISTINCT
     c.customer_unique_id,
     c.customer_city,
     c.customer_state,
-    g.geolocation_zip_code_prefix,  -- keep original column name
+    g.geolocation_zip_code_prefix,  
     g.latitude,
     g.longitude,
     current_timestamp AS record_loaded_at
 FROM {{ ref('stg_customers') }} c
-LEFT JOIN {{ ref('stg_geolocation') }} g
+LEFT JOIN {{ ref('dim_geolocation') }} g
   ON c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
